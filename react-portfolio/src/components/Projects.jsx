9ead20c98248ebff13dcc4f813072ce51b2a38d8
Lexmark444 <104227345+Lexmark444@users.noreply.github.com>
2023-11-08 21:30:20 -0500

    updated projects

--- react-portfolio/src/components/Projects.jsx
+++ react-portfolio/src/components/Projects.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import eCommerce from '../public/static/eCommerce.png'
 import spotify from '../public/static/spotify.png'
 import whiskey from '../public/static/whiskey.png'
-import library from '../public/static/library.png'
+import expense from '../public/static/ExpenseBuddy.png'
 
 export default function Projects() {
     return(
@@ -12,7 +12,19 @@ export default function Projects() {
             </h1>
             <div className='flex flex-wrap py-2 md:px-10 gap-10 justify-evenly'>
                 
-
+            <div className='flex-col lg:basis-1/3 text-center shadow-lg rounded-xl'>
+                <Image src={expense} className='object-cover' alt='Expense Buddy'/>
+                <h3 className='text-2xl text-sky-500 py-2'>ExpenseBuddy</h3>
+                <div className='flex flex-row justify-around p-10'>
+                    <p className='text-xl font-semibold hover:text-yellow-300'>
+                        <a href='https://github.com/Co-Lab-You-Belong-in-Tech/ExpenseBuddy' target='_blank'>GitHub</a>
+                    </p>
+                    <p className='text-xl font-semibold hover:text-yellow-300'>
+                        <a href='https://colab-expense-buddy.netlify.app/' target='_blank'>Site</a>
+                    </p>
+                </div>
+            </div>
+            
             <div className='flex-col lg:basis-1/3 text-center shadow-lg rounded-xl'>
                 <Image src={eCommerce} className='object-cover' alt='KrisP Corner eCommerce' />
                 <h3 className='text-2xl text-sky-500 pt-2'>KrisP Corner eCommerce</h3>
@@ -52,18 +64,7 @@ export default function Projects() {
                 </div>
             </div>
 
-            <div className='flex-col lg:basis-1/3 text-center shadow-lg rounded-xl'>
-                <Image src={library} className='object-cover' alt='library'/>
-                <h3 className='text-2xl text-sky-500 py-2'>Library App</h3>
-                <div className='flex flex-row justify-around p-10'>
-                    <p className='text-xl font-semibold hover:text-yellow-300'>
-                        <a href='https://github.com/Lexmark444/react-library' target='_blank'>GitHub</a>
-                    </p>
-                    <p className='text-xl font-semibold hover:text-yellow-300'>
-                        <a href='https://librarybookcatalog.netlify.app/' target='_blank'>Site</a>
-                    </p>
-                </div>
-            </div>
+
 
 
 
